1e57b2106badb3af691a6b41f8913ea976f0174b
Alisa Sireneva <me@purplesyringa.moe>
2024-03-29 19:44:11 +0300

    fingerprinting: Try to simplify init and add MOTD

--- tasks/fingergiving/box/shell.sh
+++ tasks/fingergiving/box/shell.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-echo "exec bash -i -l" > /usr/local/bin/shell.sh
-
-exec env -u KYZYLBORDA_SECRET_flag bash --rcfile <(echo run-parts /etc/update-motd.d) -i -l
+run-parts /etc/update-motd.d
+exec bash -i -l
